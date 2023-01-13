3783576b1bb0e940551f7cbd4d7cb644c0444605
PrzybylaHubert <hubercik022@gmail.com>
2023-01-13 13:38:50 +0100

    Add errors to ErrorList, UserChecker use ErrorList

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -2,19 +2,17 @@
 
 namespace App\Security;
 
-use App\Entity\Doctor;
-use App\Entity\Kid;
-use Symfony\Component\Security\Core\Exception\AccountExpiredException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
-use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use App\Utility\ErrorList;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 
 class UserChecker implements UserCheckerInterface
 {
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user->isActive()) {
-            throw new CustomUserMessageAccountStatusException('User inactive.');
+            throw new CustomUserMessageAccountStatusException(ErrorList::USER_INACTIVE);
         }
     }
 
